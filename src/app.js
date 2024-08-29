3970dd24733809d56fbd61d0d73cdc62c2e57aaf
Andy Almeida <73726361+andyalpa@users.noreply.github.com>
2024-08-29 16:19:03 +0000

    Domain Generator Done

--- src/app.js
+++ src/app.js
@@ -9,3 +9,24 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+const pronouns = ["the", "la", "los", "our", "your"];
+const adjs = ["great", "small", "big", "grande", "tall", "ugly-naked"];
+const nouns = ["pagina", "dog", "gato", "dragon", "guy-friends", "lastof"];
+const domains = [".com", ".us", ".org", ".net"];
+
+const getRandomElement = arr => {
+  return arr[Math.floor(Math.random() * arr.length)];
+};
+
+const generateDomain = () => {
+  let pronoun = getRandomElement(pronouns);
+  let adj = getRandomElement(adjs);
+  let noun = getRandomElement(nouns);
+  let domain = getRandomElement(domains);
+
+  document.getElementById("generateDomain").innerText =
+    pronoun + adj + noun + domain;
+};
+
+document.querySelector(".btn").addEventListener("click", generateDomain);
